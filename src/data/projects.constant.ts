cfc946a4491e6b532e81a8c1f7c32a048628e3f9
bhautik davariya <bhautikmerndeveloprs@gmail.com>
2024-05-05 11:52:42 +0530

    add project history

--- src/data/projects.constant.ts
+++ src/data/projects.constant.ts
@@ -77,7 +77,22 @@ export const ProjectsData: IProject[] = [
             'Redux(persist)',
         ],
         github: '',
-        demo: 'http://acclimentry.com/',
+        demo: 'https://app.acclimetry.com',
         image: '',
     },
+    {
+        title: 'Meta-Snap-Editor',
+        description:
+            `Meta-snap, a web application, helps users manage metadata associated with canvas headings. This user-friendly tool facilitates the creation of dynamic meta URLs, which improve search engine ranking (SEO) for their content. Built with React's flexible components, Meta-snap streamlines metadata management specifically for canvas headings. This project solidified my expertise in building React applications and provided valuable insights into SEO optimization strategies.`,
+        technologies: [
+            'React Js',
+            'Axios',
+            'JavaScript',
+            'Node Js',
+            'Redux(toolkit)',
+        ],
+        github: '',
+        demo: 'https://app.metasnap.io',
+        image: '',
+    }
 ]
